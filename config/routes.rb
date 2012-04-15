7f948cceb8c5d55c8175d21c03ade132ea8823ff
Nick Chow <nicky.chow@gmail.com>
2012-04-15 15:06:55 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
@@ -7,7 +9,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-  # match '/',        to: 'static_pages#home'
+  match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
